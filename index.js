d6e6c167e81858449692ec18496bb916831fd1f5
Evan Hahn <me@evanhahn.com>
2020-12-28 14:58:40 -0600

    Inline lib/readFile.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-const readFile = require("./lib/readFile");
+const fs = require("fs");
 const ProxyHandler = require("./lib/ProxyHandler");
+
+function readFile(file) {
+  let stats;
+
+  try {
+    stats = fs.statSync(file);
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+
+    return {};
+  }
+
+  if (stats.isFile()) {
+    const rawJson = fs.readFileSync(file, "utf8");
+
+    if (!rawJson.trim()) {
+      return {};
+    }
+
+    try {
+      return JSON.parse(rawJson);
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        throw new Error(`Could not parse ${file} as JSON`);
+      } else {
+        throw err;
+      }
+    }
+  } else {
+    throw new Error(`${file} is not a valid file`);
+  }
+}
 
 module.exports = function jsonObject(options) {
   return new Proxy(readFile(options.file), new ProxyHandler(options.file));
